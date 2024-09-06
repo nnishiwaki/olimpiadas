6ec4a2337f7f24e58d53ffc066522f356b7e5aa1
Nelson Nishiwaki <nnishiwaki@yahoo.com>
2024-09-06 20:51:38 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,20 +1,29 @@
 let dados = [
     {
-        titulo: "Rayssa Leal",
-        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
-        tags: "skate fada sk8"
+        candidato: "Pablo Marçal (PRTB)",
+        proposta: "Proposta: O empresário e influenciador Pablo Marçal diz em seu plano de governo que pretende levar ``São Paulo para 2050 em 2028´´, ano do término do mandato do próximo gestor da capital paulista. A ideia é semelhante a do ex-presidente Juscelino Kubitschek, que divulgava seu plano de metas com o slogan ``50 anos em cinco´´. O documento com propostas registrado por Marçal na Justiça Eleitoral possui cem páginas ao todo. No entanto, excluindo páginas inteiras com fotos, outros materiais gráficos de campanha e páginas duplicadas, o número de folhas cai a 77. Uma das propostas apresentadas pelo ex-coach é a de incluir câmeras de monitoramento em caminhões de lixo. Com isso, o postulante pretende “monitorar a coleta e o descarte adequados, assegurando a manutenção da limpeza e segurança nas áreas urbanas”. Na educação, Marçal quer a “capacitação contínua” do quadro de funcionários, além de usar tecnologias no ensino. Sobre melhoria nos índices de alfabetização, ele promete um sistema de remuneração extra aos profissionais, baseado no resultado obtido. Na segurança, o empresário quer ampliar o uso de drones em ações preventivas. Para a mobilidade urbana, o empresário sugere a construção de um teleférico nas periferias. Outra obra citada no plano do candidato é a construção de um arranha-céu de mais de mil metros de altura, para consolidar a imagem de São Paulo como ``metrópole de destaque mundial´´. Na saúde, o ex-coach quer mais programas da Prefeitura com enfoque em saúde mental, da mulher e especializada em idosos. O ex-coach também quer firmar parcerias na área com universidades e centros de pesquisas e reduzir a fila de espera para exames na rede municipal.",
+        profissao: "Profissão: Empresário",
+        patrimonio: "Patrimônio: R$ 169.503.058,00",
+        instrucao: "Instrução: Superior completo",
+        link: "https://valor.globo.com/politica/eleicoes-2024/noticia/2024/09/05/quem-e-pablo-marcal-candidato-do-prtb-a-prefeito-de-sao-paulo.ghtml",
+        tags: "influenciador coach teleferico parcerias prtb"
     },
     {
-        titulo: "Beatriz Souza",
-        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
-        tags: "judô judo judoca judoka"
+        candidato: "Ricardo Nunes (MDB)",
+        proposta: "Proposta: Candidato à reeleição, o plano de governo do prefeito de São Paulo Ricardo Nunes foca na continuidade e na ampliação de políticas públicas já adotadas em sua gestão. O tom adotado é generalista e pouco específico. O programa foi coordenado por Rodrigo Garcia (sem partido), ex-governador do Estado. Na área de saúde, Nunes propõe dar seguimento à expansão da rede de Unidades Básicas de Saúde (UBS), além de investir na modernização e ampliação dos hospitais municipais e aumentar a disponibilidade de especialidades médicas nas UBSs. Para a segurança, Nunes apresenta propostas pouco detalhadas. Há a menção de reduzir a vulnerabilidade dos jovens ao crime organizado por meio da ampliação do acesso às atividades educacionais, culturais e esportivas, além da promessa de ampliar ações protetivas às mulheres, crianças e idosos vítimas de violência. Na educação, o prefeito promete manter a fila zerada nas creches, além de ampliar o ensino em tempo integral nas escolas municipais e dar continuidade ao programa “Aprender e Ensinar”. O mandatário também quer ampliar os processos formativos para professores e gestores e lançar planos de ação com foco em alfabetização.",
+        profissao: "Profissão: Prefeito",
+        patrimonio: "Patrimônio: R$ 4.843.350,00",
+        instrucao: "Instrução: Superior incompleto",
+        link: "https://valor.globo.com/politica/eleicoes-2024/noticia/2024/09/05/quem-e-ricardo-nunes-candidato-do-mdb-a-prefeito-de-sao-paulo.ghtml",
+        tags: "mdb ubs crime creches reeleicao"
     },
     {
-        titulo: "Rebeca Andrade",
-        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
-        tags: "ginástica ginastica ouro"
+        candidato: "Guilherme Boulos (PSOL)",
+        proposta: "Proposta: O deputado federal Guilherme Boulos elencou 119 propostas para São Paulo em seu plano de governo. As medidas estão distribuídas em 27 eixos, como saúde, educação, segurança e juventude. Nas propostas para a segurança urbana, consta um reforço da ronda escolar, com a presença de viaturas da Guarda Civil Metropolitana (GCM) na porta de cada escola municipal durante a entrada e a saída dos alunos. Além disso, Boulos prometeu dobrar o efetivo da guarda do município, que, hoje, conta com 7 mil agentes. O candidato do PSOL também pretende implementar câmeras corporais na guarda e identificar as rondas em bairros e escolas. Na área de transportes, Boulos promete que, se eleito, expandirá a tarifa zero nos ônibus. Como mostrou o Estadão, outra proposta do psolista é um programa de subsídio de aluguel. Com o título de “Primeiro Escritório”, o programa faz parte de um conjunto de propostas para adensar o centro da cidade. Para a educação, a candidatura do PSOL quer ampliar a rede em tempo integral e garantir formação continuada aos educadores, além de investir em novas condições de trabalho e segurança aos professores. Na saúde, Boulos quer priorizar o atendimento a questões de saúde mental, além de garantir o abastecimento da rede municipal com medicamentos e criar, nas periferias, 16 equipamentos intitulados de “Poupatempo da Saúde”, para atendimento policlínico.",
+        profissao: "Profissão: Deputado",
+        patrimonio: "Patrimônio: R$ 199.596,00",
+        instrucao: "Instrução: Superior completo",
+        link: "https://valor.globo.com/politica/eleicoes-2024/noticia/2024/09/05/quem-e-guilherme-boulos-candidato-do-psol-a-prefeito-de-sao-paulo.ghtml",
+        tags: "psolista psol ronda gcm medicamentos"
     }
 ];
